75d5ed8a75a5859067f87f4df82636fd3d5dae31
Netra Chandrasekhar <47250682+Netra24@users.noreply.github.com>
2021-03-02 13:26:34 +0530

    Update UserRepository.kt

--- app/src/main/java/com/android/happilyeverapp/dataModels/roomDb/UserRepository.kt
+++ app/src/main/java/com/android/happilyeverapp/dataModels/roomDb/UserRepository.kt
@@ -6,19 +6,27 @@ class UserRepository(private val userDao: UserDao) {
 
     val getUsers: LiveData<List<User>> = userDao.getUsers()
 
-    suspend fun signup(user: User) {
-        userDao.signup(user)
+    suspend fun signup(user: User) : Long{
+        return userDao.signup(user)
     }
+
+//    suspend fun signup(user: User){
+//        userDao.signup(user)
+//    }
 
     suspend fun login(email: String, pwd: String): User {
         return userDao.login(email, pwd)
     }
 
-    suspend fun getUser(email: String): User {
-        return userDao.getUser(email)
+    suspend fun getUser(id: Long): User {
+        return userDao.getUser(id)
     }
 
     suspend fun updateProfile(user: User) {
         userDao.updateProfile(user)
+    }
+
+    suspend fun getUsers(): LiveData<List<User>>{
+        return userDao.getUsers()
     }
 }
