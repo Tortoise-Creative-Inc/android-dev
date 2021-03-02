60aa8abe0e0fa34ed8f10dcc509443e8ec9c703e
Netra Chandrasekhar <47250682+Netra24@users.noreply.github.com>
2021-03-02 13:27:37 +0530

    Update UserDao.kt

--- app/src/main/java/com/android/happilyeverapp/dataModels/roomDb/UserDao.kt
+++ app/src/main/java/com/android/happilyeverapp/dataModels/roomDb/UserDao.kt
@@ -7,7 +7,10 @@ import androidx.room.*
 interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun signup(user: User)
+    suspend fun signup(vararg user: User)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun signup(user: User) : Long
 
     @Query("SELECT * FROM user_auth ORDER BY id ASC")
     fun getUsers() : LiveData<List<User>>
@@ -15,8 +18,8 @@ interface UserDao {
     @Query("SELECT * FROM user_auth WHERE email= :userEmail AND password= :userPwd")
     fun login(userEmail :String, userPwd :String) : User
 
-    @Query("SELECT * FROM user_auth WHERE email= :userEmail")
-    fun getUser(userEmail: String) : User
+    @Query("SELECT * FROM user_auth WHERE id= :id")
+    fun getUser(id: Long) : User
 
     @Update
     fun updateProfile(user: User)
